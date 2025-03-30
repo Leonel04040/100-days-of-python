31c1b2317483c7117ebd5d8643afde9c46e2683c
Leonel <99699505+Leonel04040@users.noreply.github.com>
2025-03-30 12:01:15 -0600

    Create Day 1_variables.py

---
+++ Day 1_variables.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+print("The name",name +" " "has")
+print(len(name),"letters")
+
+username = input("What's your username")
+length = len(username)
+
+print(username,length)
