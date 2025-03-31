79434340a2f657a2c0673a5ab1c8ea4ffbe02841
Leonel <99699505+Leonel04040@users.noreply.github.com>
2025-03-31 10:46:05 -0600

    Create Day 2_types_and_numbers.py

---
+++ Day 2_types_and_numbers.py
@@ -0,0 +1,12 @@
+#type conversions
+x=input("Whats your name?")
+length_name=len(x)
+print("Number of letters in your name: "+ str(length_name))
+print(type(length_name))
+
+#Number manipulation
+score = 0
+height= 1.8
+is_winnig= True
+print(6 + 4 / 2 - (1 * 2))
+print(f"your score is = {score}, your height is {height}, You are wining is {is_winnig}")
